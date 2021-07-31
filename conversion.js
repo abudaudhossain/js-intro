0e9b0935c5e27d6173a5e77f4dbf5c45418d3f2e
abudaudhossain <abudaudhossainsumon@gmail.com>
2021-07-31 11:24:57 +0600

    add conversion

---
+++ conversion.js
@@ -0,0 +1,14 @@
+var firstName ='Abu Daud';
+var lastName = 'Hossain'
+var fullName = firstName + ' ' + lastName ;
+console.log(fullName);
+
+var onionPrice = "45";
+var eggPrice = "23.566";
+console.log(onionPrice);
+var onionPriceNumber = parseInt(onionPrice);
+console.log(onionPriceNumber);
+var eggPriceNumber = parseInt(eggPrice);
+console.log(eggPriceNumber);
+
+console.log(parseFloat(eggPrice));
