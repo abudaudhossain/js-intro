993511d1518210a353db0a6ba4a590638aadc946
abudaudhossain <abudaudhossainsumon@gmail.com>
2021-07-31 08:43:52 +0600

    add js introction

---
+++ frist.js
@@ -0,0 +1,13 @@
+console.log(7);
+
+var price = 21;
+var age = 13;
+var temperature = 37;
+
+var name = "Kosur Uddin";
+var address = "vooter gram";
+var gf = "Alia Bhatt";
+var school = 'Rowmari C G Jaman High School';
+
+var pass = true;
+var subscribed = false;
---
+++ math-oparetion.js
@@ -0,0 +1,10 @@
+var onionPrices = 43;
+var eggPrices = 41;
+
+var totalPrice = onionPrices + eggPrices;
+var priceDefferant = onionPrices - eggPrices;
+var priceMult = onionPrices * 4;
+
+console.log(totalPrice);
+console.log(priceDefferant);
+console.log(priceMult);
